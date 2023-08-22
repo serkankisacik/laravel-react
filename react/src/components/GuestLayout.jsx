f7212fc6cd03a2d266e034bb384b604081f23d9c
Serkan Kisacik <serkankisacik01@gmail.com>
2023-08-22 07:34:17 +0300

    Giriş yap işlemleri
    localStorage.getItem('ACCESS_TOKEN')

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,6 +1,12 @@
-import {Outlet} from "react-router-dom";
+import {Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function GuestLayout() {
+    const {token} = useStateContext();
+
+    if (token) {
+        return <Navigate to="/"/>
+    }
     return (
         <div>
             <Outlet/>
