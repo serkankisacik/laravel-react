5174af682ed769459b232b8ad938b6fc6f065979
Serkan Kisacik <serkankisacik01@gmail.com>
2023-09-05 01:08:39 +0300

    create requests

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class LoginRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class LoginRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,9 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
-        ];
+            'email' => 'required|email|exists:users,email',
+            'password' => [
+                'required',
+            ];
     }
 }
