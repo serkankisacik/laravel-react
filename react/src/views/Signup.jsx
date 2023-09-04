77250f9880baf90ead30fded2811b69d0a088453
Serkan Kisacik <serkankisacik01@gmail.com>
2023-09-05 00:20:41 +0300

    Signup

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,25 @@
+import {Link} from "react-router-dom";
+
 export default function Signup() {
+
+    const onSubmit = (ev) =>{
+        ev.preventDefault()
+    }
     return (
-        <div>
-            Signup
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">Signup or free</h1>
+                    <input type={"text"} placeholder="Full Name"/>
+                    <input type={"email"} placeholder="Email Address"/>
+                    <input type={"password"} placeholder="Password"/>
+                    <input type={"password"} placeholder="Password Confirmation"/>
+                    <button className="btn btn-block">Signup</button>
+                    <p className="message">
+                        Already Registered? <Link to={"/login"}>Sign in</Link>
+                    </p>
+                </form>
+            </div>
         </div>
     )
 }
