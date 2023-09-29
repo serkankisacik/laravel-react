63d0aa05a8eccf659679233f85ff94671d907617
Serkan Kisacik <serkankisacik01@gmail.com>
2023-09-30 00:15:01 +0300

    User back-end
    02:02

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,25 @@
+import {useEffect, useState} from "react";
+import axiosClient from "../axios-client.js";
+
 export default function Users() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({data}) => {
+                setLoading(false)
+                console.log(data)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }
     return (
         <div>
             Users
