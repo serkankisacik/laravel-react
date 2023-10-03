84c60f5b0c8681f97eeef1f8ac94960a4bbf3030
Serkan Kisacik <serkankisacik01@gmail.com>
2023-10-03 23:42:17 +0300

    Son notification

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,11 +1,12 @@
 import {useEffect, useState} from "react";
 import axiosClient from "../axios-client.js";
 import {Link} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Users() {
     const [users, setUsers] = useState([]);
     const [loading, setLoading] = useState(false);
-
+    const {setNotification} = useStateContext();
     useEffect(() => {
         getUsers();
     }, []);
@@ -17,7 +18,7 @@ export default function Users() {
 
         axiosClient.delete(`/users/${u.id}`)
             .then(() => {
-                //TODO show notification
+                setNotification("User was successfully deleted");
                 getUsers()
             })
     }
